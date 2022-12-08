1369e2c3007e49df5abc0415e63ca5b4012d4b19
Tina Le <tinale218@gmail.com>
2022-12-07 22:14:23 -0500

    added scroll to top button + functionality

--- js/script.js
+++ js/script.js
@@ -31,3 +31,22 @@ window.onload=()=>{
     }
 };
 
+let mybutton = document.getElementById("back2top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
